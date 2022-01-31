865143e4d0b5c2901ba34bc657dd9207b02f519d
Mankunian <dias.07.04@gmail.com>
2022-01-31 23:35:22 +0600

    update aside

--- src/app/modules/dashboard/components/club/administration-staff/administration-staff.component.ts
+++ src/app/modules/dashboard/components/club/administration-staff/administration-staff.component.ts
@@ -11,9 +11,9 @@ export class AdministrationStaffComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.admins = [
-			{ id: 1, name: 'Roman Abramovic', img: '1.jpg', position: 'Президент', social_links: [] },
+			{ id: 1, name: 'Roman Abramovic', img: '1.jpg', position: 'Президент' },
 			{ id: 2, name: 'Alex Ferguson', img: '2.jpg', position: 'Генеральный директор', social_links: [] },
-			{ id: 3, name: 'МУХАММЕД ИБН САЛМАН', img: '3.jpg', position: 'Финансовый директор', social_links: [] },
+			{ id: 3, name: 'Mohamed Salman', img: '3.jpg', position: 'Финансовый директор', social_links: [] },
 		];
 	}
 
